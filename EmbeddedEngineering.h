1e2e58adf26806a8b5684f6d96c6f0b5f27bc540
Wokewok <cory@corycline.com>
2022-12-09 18:45:29 -0600

    Create EmbeddedEngineering.h
    merica

---
+++ EmbeddedEngineering.h
@@ -0,0 +1,23 @@
+/** @file EmbeddedEngineering.h.h
+ * 
+ * @brief A description of the module’s purpose. 
+ *
+ * @par       
+ * COPYRIGHT NOTICE: (c) 2018 Barr Group.  All rights reserved.
+ */ 
+
+#ifndef EMBEDDEDENGINEERING_H
+#define EMBEDDEDENGINEERING_H
+
+#define whilst while
+#define should if
+#define be_more_grandiose_than >
+#define be_less_grandiose_than <
+#define level =
+#define proceedeth continue
+#define going_back return
+#define steal malloc
+#define unexist free
+#define beareth_interest(x) x++
+
+#endif /* EMBEDDEDENGINEERING_H */
